d3ebe5b092788107803885c7b60d80af13955c3b
muschellij2 <muschellij2@gmail.com>
2020-01-14 09:04:55 -0500

    updated the docs

---
+++ R/pcm_to_wav (MacBook-Pro-9.local's conflicted copy 2020-01-14).R
@@ -0,0 +1,44 @@
+#' Convert PCM to Wav
+#'
+#' @param input output from [aws.polly::get_synthesis] or PCM filename
+#' @param output output file for Wav file
+#' @param extensible passed to [tuneR::writeWave]
+#' @param sample_rate Sampling rate for [tuneR::Wave]
+#' @return A filename of the output
+#' @export
+#' @examples
+#' fname = system.file("extdata", "pcm_file.wav", package = "text2speech")
+#' res = pcm_to_wav(fname)
+#' testthat::expect_error(tuneR::readWave(fname))
+#' testthat::expect_is(tuneR::readWave(res), "Wave")
+#'
+#' \dontrun{
+#' if (requireNamespace("aws.polly", quietly = TRUE)) {
+#' text = "hey, ho, let's go!"
+#' if (tts_amazon_auth()) {
+#'    res = tts_amazon(text, output_format = "wav")
+#' }
+#' }
+#' }
+pcm_to_wav = function(
+  input,
+  output = tempfile(fileext = ".wav"),
+  sample_rate = 16000,
+  extensible = FALSE) {
+
+
+  tfile = tempfile(fileext = ".pcm")
+  if (is.raw(input)) {
+    writeBin(input, con = tfile)
+    input = tfile
+  }
+  stopifnot(file.exists(input))
+  sz = file.size(input)
+  buffer = 1000
+  pcm <- readBin(input, what = integer(),
+                 n = 2 * sz + buffer,
+                 size = 2, endian = "little", signed = TRUE)
+  wav = tuneR::Wave(pcm, samp.rate = sample_rate, bit = 16, pcm = TRUE)
+  tuneR::writeWave(object = wav, filename = output, extensible = extensible)
+  return(output)
+}
---
+++ tests/testthat/test-voices (MacBook-Pro-9.local's conflicted copy 2020-01-14).R
@@ -0,0 +1,34 @@
+testthat::context("List the Voices")
+
+fixed_names = c("voice", "language", "language_code",
+                "gender", "service")
+
+testthat::test_that(
+  "Microsoft Voices", {
+    df = tts_voices(service = "microsoft")
+    testthat::expect_is(df, "data.frame")
+    testthat::expect_named(df, fixed_names)
+  }
+)
+
+testthat::test_that(
+  "Google Voices", {
+    if (tts_google_auth()) {
+      df = tts_voices(service = "google")
+      testthat::expect_is(df, "data.frame")
+      testthat::expect_named(df, fixed_names)
+    }
+  }
+)
+
+if (requireNamespace("aws.polly", quietly = TRUE)) {
+  testthat::test_that(
+    "Amazon Voices", {
+      if (tts_amazon_auth()) {
+        df = tts_voices(service = "amazon")
+        testthat::expect_is(df, "data.frame")
+        testthat::expect_named(df, fixed_names)
+      }
+    }
+  )
+}
