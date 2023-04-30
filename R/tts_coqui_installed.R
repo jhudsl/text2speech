3aef4992342105588b1d66d6c364c6e9692a950e
howardbaek <howardbaek.fh@gmail.com>
2023-04-29 22:20:10 -0700

    Check whether coqui "tts" was installed

--- R/tts_coqui_installed.R
+++ R/tts_coqui_installed.R
@@ -1,12 +1,14 @@
 tts_coqui_installed <- function() {
   coqui_assert()
   coqui_path <- getOption("path_to_coqui")
+  coqui_path_processed <- gsub("/tts$", "", coqui_path)
 
-  res <- withr::with_path("/opt/homebrew/Caskroom/miniforge/base/bin",
-                   system("which tts"))
-  if (res == 0) {
-    return(TRUE)
-  } else {
+  res <- withr::with_path(coqui_path_processed,
+                          system("which tts", intern = TRUE))
+
+  if (!is.null(attr(res, "status")) && attr(res, "status") == 1) {
     return(FALSE)
+  } else {
+    return(TRUE)
   }
 }
