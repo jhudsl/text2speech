636f3dfbed95a1c026c0aa57b52bcae897049a1e
howardbaek <howardbaek.fh@gmail.com>
2023-05-01 16:14:21 -0700

    Begin testing `tts_coqui()`

--- tests/testthat/test-tts_coqui.R
+++ tests/testthat/test-tts_coqui.R
@@ -1,9 +1,26 @@
-testthat::test_that("coqui TTS works", {
-      df = tts("Hi Candace, this is Howard.", service = "coqui")
-      testthat::expect_is(df, "data.frame")
-      testthat::expect_named(df, fixed_names)
-      testthat::expect_is(df$wav[[1]], "Wave")
-      wav = df$wav[[1]]
+fixed_names = c("index", "original_text", "text", "wav",
+                "file", "audio_type", "duration", "service")
+
+testthat::test_that("Vanilla coqui TTS works", {
+      x = tts("Algorithmic complexity is a key consideration when
+               designing efficient solutions for large-scale data processing",
+               service = "coqui")
+      testthat::expect_s3_class(x, "data.frame")
+      testthat::expect_named(x, fixed_names)
+      testthat::expect_s4_class(x$wav[[1]], "Wave")
+      wav = x$wav[[1]]
       testthat::expect_true(length(wav)/wav@samp.rate >= 0.5)
   }
+)
+
+testthat::test_that("coqui TTS works", {
+  x = tts("Algorithmic complexity is a key consideration when
+               designing efficient solutions for large-scale data processing",
+          service = "coqui")
+  testthat::expect_s3_class(x, "data.frame")
+  testthat::expect_named(x, fixed_names)
+  testthat::expect_s4_class(x$wav[[1]], "Wave")
+  wav = x$wav[[1]]
+  testthat::expect_true(length(wav)/wav@samp.rate >= 0.5)
+}
 )
