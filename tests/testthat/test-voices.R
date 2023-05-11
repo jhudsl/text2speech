5bd5ac96c92fed233ed8844fe8ebc2ca26604c0e
howardbaek <howardbaek.fh@gmail.com>
2023-05-11 14:20:36 -0700

    Get rid of testing for Microsoft voices

--- tests/testthat/test-voices.R
+++ tests/testthat/test-voices.R
@@ -2,14 +2,6 @@ fixed_names = c("voice", "language", "language_code",
                 "gender", "service")
 
 testthat::test_that(
-  "Microsoft Voices", {
-    df = tts_voices(service = "microsoft")
-    testthat::expect_is(df, "data.frame")
-    testthat::expect_named(df, fixed_names)
-  }
-)
-
-testthat::test_that(
   "Google Voices", {
     if (tts_google_auth()) {
       df = tts_voices(service = "google")
