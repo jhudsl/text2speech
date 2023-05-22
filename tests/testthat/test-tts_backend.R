dad28954240e203a5b9f3e2654084e5722d85278
howardbaek <howardbaek.fh@gmail.com>
2023-05-22 15:56:31 -0700

    Write tests for `tts_voices()`

--- tests/testthat/test-tts_backend.R
+++ tests/testthat/test-tts_backend.R
@@ -1,42 +1,33 @@
-patrick::with_parameters_test_that("tts() returns a data.frame",
-                                   {
-                                     if (tts_auth) {
-                                       response_df = tts("Algorithmic complexity is a key consideration
-                                                when designing efficient solutions for large-scale data processing",
-                                                         service = company)
-                                       testthat::expect_s3_class(response_df, char_value)
-                                     }
-                                   },
-                                   tts_auth = c(tts_amazon_auth(), tts_google_auth(), tts_microsoft_auth(region = "westus")),
-                                   company  = c("amazon", "google", "microsoft"),
-                                   char_value = "data.frame"
-)
-
-patrick::with_parameters_test_that("tts() returns a data.frame with an Wave object",
+patrick::with_parameters_test_that("test tts() on Amazon, Google, and Microsoft engines) ",
                                    {
                                      if (tts_auth) {
                                        response_df = tts("Algorithmic complexity is a key consideration
                                                 when designing efficient solutions for large-scale data processing",
                                                          service = company)
                                        audio_value = response_df$wav[[1]]
-                                       testthat::expect_s4_class(audio_value, char_value)
-                                     }
-                                   },
-                                   tts_auth = c(tts_amazon_auth(), tts_google_auth(), tts_microsoft_auth(region = "westus")),
-                                   company  = c("amazon", "google", "microsoft"),
-                                   char_value = "Wave"
-)
-
-patrick::with_parameters_test_that("tts() successfully created an audio output in a file path",
-                                   {
-                                     if (tts_auth) {
-                                       response_df = tts("Algorithmic complexity is a key consideration
-                                                when designing efficient solutions for large-scale data processing",
-                                                         service = company)
                                        audio_path = response_df$file[[1]]
+
+                                       testthat::expect_s3_class(response_df, "data.frame")
+                                       testthat::expect_s4_class(audio_value, "Wave")
                                        testthat::expect_equal(file.exists(audio_path), TRUE)
                                      }
                                    },
                                    tts_auth = c(tts_amazon_auth(), tts_google_auth(), tts_microsoft_auth(region = "westus")),
                                    company  = c("amazon", "google", "microsoft")
+)
+
+fixed_names = c("index", "original_text", "text", "wav",
+                "file", "audio_type", "duration", "service")
+
+test_that("test tts() on Coqui engine", {
+  response_df = tts("Algorithmic complexity is a key consideration when
+               designing efficient solutions for large-scale data processing",
+                    service = "coqui")
+  # Check x is a data.frame
+  expect_s3_class(response_df, "data.frame")
+  # Check column names
+  expect_named(response_df, fixed_names)
+  # Check Wave
+  expect_s4_class(response_df$wav[[1]], "Wave")
+}
 )
--- tests/testthat/test-tts_voices.R
+++ tests/testthat/test-tts_voices.R
@@ -1,24 +1,25 @@
 fixed_names = c("voice", "language", "language_code",
                 "gender", "service")
 
-testthat::test_that(
-  "Google Voices", {
-    if (tts_google_auth()) {
-      df = tts_voices(service = "google")
-      testthat::expect_is(df, "data.frame")
-      testthat::expect_named(df, fixed_names)
-    }
-  }
+patrick::with_parameters_test_that("test tts_voices() on Amazon, Google, and Microsoft engines",
+                                   {
+                                     if (tts_auth) {
+                                       response_df = tts_voices(service = company)
+                                       testthat::expect_s3_class(response_df, "data.frame")
+                                       testthat::expect_named(response_df, fixed_names)
+                                     }
+                                   },
+                                   tts_auth = c(tts_amazon_auth(), tts_google_auth(), tts_microsoft_auth(region = "westus")),
+                                   company  = c("amazon", "google", "microsoft")
 )
 
-if (requireNamespace("aws.polly", quietly = TRUE)) {
-  testthat::test_that(
-    "Amazon Voices", {
-      if (tts_amazon_auth()) {
-        df = tts_voices(service = "amazon")
-        testthat::expect_is(df, "data.frame")
-        testthat::expect_named(df, fixed_names)
-      }
-    }
-  )
+fixed_names_coqui <- c("language", "dataset", "model_name")
+
+test_that("test tts_voices() on Coqui engine", {
+  response_df = tts_voices(service = "coqui")
+  # Check x is a data.frame
+  expect_s3_class(response_df, "data.frame")
+  # Check column names
+  expect_named(response_df, fixed_names_coqui)
 }
+)
