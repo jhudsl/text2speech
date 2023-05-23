bf5668e934b9d6d4436036be9ccf3529f5c3c8d5
howardbaek <howardbaek.fh@gmail.com>
2023-05-23 11:57:54 -0700

    Put tts coqui engine inside parametrized test

--- tests/testthat/test-tts_backend.R
+++ tests/testthat/test-tts_backend.R
@@ -12,22 +12,6 @@ patrick::with_parameters_test_that("test tts() on Amazon, Google, and Microsoft 
                                        testthat::expect_equal(file.exists(audio_path), TRUE)
                                      }
                                    },
-                                   tts_auth = c(tts_amazon_auth(), tts_google_auth(), tts_microsoft_auth(region = "westus")),
-                                   company  = c("amazon", "google", "microsoft")
-)
-
-fixed_names = c("index", "original_text", "text", "wav",
-                "file", "audio_type", "duration", "service")
-
-test_that("test tts() on Coqui engine", {
-  response_df = tts("Algorithmic complexity is a key consideration when
-               designing efficient solutions for large-scale data processing",
-                    service = "coqui")
-  # Check x is a data.frame
-  expect_s3_class(response_df, "data.frame")
-  # Check column names
-  expect_named(response_df, fixed_names)
-  # Check Wave
-  expect_s4_class(response_df$wav[[1]], "Wave")
-}
+                                   tts_auth = c(tts_amazon_auth(), tts_google_auth(), tts_microsoft_auth(region = "westus"), TRUE),
+                                   company  = c("amazon", "google", "microsoft", "coqui")
 )
